4b13edaeac27933e9702b8ec0bb3bb296495d234
Maxime Veber <nek.dev@gmail.com>
2019-01-02 20:08:34 +0100

    Add new ApiResponse "Ok" to Happii
    Ok response is a standard 200. It can be extended.
    Nothing special here: it's normalized by the standard
    object normalizer of Symfony.

---
+++ src/Response/Ok.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Biig\Happii\Response;
+
+
+class Ok implements ApiResponse
+{
+    /** @var string */
+    private $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function httpStatus(): int
+    {
+        return 200;
+    }
+}
