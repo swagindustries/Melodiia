7c89d71ccf3c64535dd4e333ca28eb27c21f57b2
Nek (Maxime Veber) <nek.dev@gmail.com>
2019-01-20 19:47:56 +0100

    Adds some code to support VO and domain models
    By default Sf forms are not supporting VO / Domain models.
    
    This methods default configuration adds support for Value Objects
    and Domain Models.
    
    Here comes also a little fix for test suite.

--- src/Bridge/Symfony/Form/Type/ApiType.php
+++ src/Bridge/Symfony/Form/Type/ApiType.php
@@ -3,12 +3,60 @@
 namespace Biig\Melodiia\Bridge\Symfony\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ApiType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(['csrf_protection' => false]);
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+
+            /*
+             * Creates data object just like standard form would do
+             * but used constructor with given data.
+             *
+             * @param FormInterface $form
+             * @return null|object
+             * @throws \ReflectionException
+             */
+            'empty_data' => function (FormInterface $form) {
+                $dataClass = $form->getConfig()->getOption('data_class');
+
+                if (!class_exists($dataClass)) {
+                    return null;
+                }
+
+                $ref = new \ReflectionClass($dataClass);
+                $constructorParameters = $ref->getConstructor()->getParameters();
+
+                // Case of anemic model, we have nothing to do here.
+                if (count($constructorParameters) < 1) {
+                    return new $dataClass();
+                }
+
+                $constructorData = [];
+                foreach ($constructorParameters as $parameter) {
+                    if ($form->has($parameter->getName())) {
+                        if (null === $form->get($parameter->getName())->getData() && !$parameter->allowsNull() && !$parameter->isDefaultValueAvailable()) {
+                            return null;
+                        }
+
+                        $data = $form->get($parameter->getName())->getData();
+                    } else {
+                        $data = null;
+                    }
+
+                    if (empty($data) && $parameter->isDefaultValueAvailable()) {
+                        $data = $parameter->getDefaultValue();
+                    }
+
+                    $constructorData[] = $data;
+                }
+
+                return new $dataClass(...$constructorData);
+            },
+        ]);
     }
 }
