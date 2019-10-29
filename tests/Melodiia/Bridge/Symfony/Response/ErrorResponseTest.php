9ac898818b6d2963d7b9fe72cd08dd5b1b128737
Kevin Grenier <kevin.grenier@biig.fr>
2019-10-29 17:34:12 +0100

    Add custom error response

---
+++ tests/Melodiia/Bridge/Symfony/Response/ErrorResponseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Biig\Melodiia\Bridge\Symfony\Response;
+
+use Biig\Melodiia\Response\AbstractUserDataErrorResponse;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ErrorResponseTest extends TestCase
+{
+    public function testItIsABadRequestHttpResponse()
+    {
+        $errorResponse = new ErrorResponse([]);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $errorResponse->httpStatus());
+    }
+
+    public function testItReturnAResponseExtendingRightClass()
+    {
+        $errorResponse = new ErrorResponse([]);
+        $this->assertInstanceOf(AbstractUserDataErrorResponse::class, $errorResponse);
+    }
+
+    public function testItContainErrorAtSameLevel()
+    {
+        $errorResponse = new ErrorResponse(['hello', 'im', 'an', 'error']);
+        $this->assertCount(1, $errorResponse->getErrors());
+        $errors = $errorResponse->getErrors()[0]->getErrors();
+        $this->assertCount(4, $errors);
+    }
+
+    public function ifIGiveAnErrorKeyItShouldUseIt()
+    {
+        $errorResponse = new ErrorResponse([], 'angry_error');
+        $this->assertEquals('angry_error', $errorResponse->getErrors()[0]->getPropertyPath());
+    }
+}
