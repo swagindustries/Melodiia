7d95b78f5390791848a77c48d7e4fefd28e25a21
Nek (Maxime Veber) <nek.dev@gmail.com>
2020-04-04 01:45:17 +0200

    Fix #36 by adding a CollectionType
    Problem:
    The Symfony CollectionType is cool in the context of Forms. But when it
    comes to API new problems comes:
    1. Data order (because the order of data input is not predictable)
    2. Id management (because we need ids to re-order, but they are not in
       the form type
    3. We do not need all the front-end related stuff tha the Sf collection
       supports
    
    Solution:
    Adding a custom CollectionType dedicated to Melodiia usages.

---
+++ src/Bridge/Symfony/Form/Listener/ReorderDataToMatchCollectionListener.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Biig\Melodiia\Bridge\Symfony\Form\Listener;
+
+use Biig\Melodiia\Crud\CrudableModelInterface;
+use Biig\Melodiia\Exception\MelodiiaLogicException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\FormEvents;
+
+class ReorderDataToMatchCollectionListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            // (PRE SUBMIT of ResizeFormListener must comes after)
+            FormEvents::PRE_SUBMIT => ['preSubmit', 10],
+        ];
+    }
+
+    public function preSubmit(PreSubmitEvent $event)
+    {
+        $data = $event->getData();
+        if (empty($data)) {
+            return;
+        }
+
+        $form = $event->getForm();
+        $dataOrdered = [];
+
+        // We need to know the last index of the collection.
+        // We cannot do incremental index for data because the last index may be deleted.
+        $lastCollectionIndex = 0;
+        foreach ($form as $name => $child) {
+            $lastCollectionIndex = $name;
+            $item = $child->getData();
+            if (!$item instanceof CrudableModelInterface) {
+                throw new MelodiiaLogicException(sprintf(
+                    'Impossible manage the model of type "%s" inside Melodiia because it does not implement CrudableModelInterface.',
+                    get_class($item)
+                ));
+            }
+            $itemId = (string) $item->getId();
+            foreach ($data as $inputItem) {
+                if (!isset($inputItem['id'])) {
+                    continue;
+                }
+                if ($inputItem['id'] === $itemId) {
+                    $dataOrdered[$name] = $inputItem;
+                    break; // This break removes duplicated items (based on id) inside input data
+                }
+            }
+        }
+
+        // Add "new" items (without id) to the data ordered
+        foreach ($data as $inputItem) {
+            if (!isset($inputItem['id'])) {
+                $lastCollectionIndex++;
+                $dataOrdered[$lastCollectionIndex] = $inputItem;
+            }
+        }
+
+        // Removing ids, they are just here for this listener and are not part of the form.
+        foreach ($dataOrdered as $index => $item) {
+            if (isset($dataOrdered[$index]['id'])) {
+                unset($dataOrdered[$index]['id']);
+            }
+        }
+
+        $event->setData($dataOrdered);
+    }
+}
