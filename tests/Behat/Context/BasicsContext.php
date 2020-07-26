1fbed57e2b2a78d9030c771914c269f2bf7e91d5
Maxime Veber <nek.dev@gmail.com>
2020-07-26 12:20:33 +0200

    feat(test): add behat tests
    This commit adds the first functional tests.

---
+++ tests/Behat/Context/BasicsContext.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace SwagIndustries\Melodiia\Tests\Behat\Context;
+
+use Behat\Gherkin\Node\PyStringNode;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class BasicsContext extends AbstractContext
+{
+    private $response;
+
+    /**
+     * @When I make a GET request on :uri
+     * @Given I make a :verb request on :uri with the content:
+     */
+    public function iMakeARequestOn($uri, $verb = 'GET', PyStringNode $content = null)
+    {
+        $client = $this->getContainer()->get('test.client');
+
+        if ('GET' === $verb) {
+            $client->request($verb, $uri);
+            $this->response = $client->getResponse();
+
+            return;
+        }
+
+        if (empty($content)) {
+            throw new \Exception(sprintf('Cannot process request "%s" with no content.', $verb));
+        }
+
+        // Just validate json
+        json_decode($content->getRaw(), true, 512, JSON_THROW_ON_ERROR);
+
+        $client->request($verb, $uri, [], [], [], $content);
+        $this->response = $client->getResponse();
+    }
+
+    /**
+     * @Then I should retrieve:
+     * @Given the last response contains:
+     */
+    public function iShouldRetrieve(PyStringNode $string)
+    {
+        $json = $this->response->getContent();
+        $expected = \json_encode(\json_decode($string->getRaw(), true, 512, JSON_THROW_ON_ERROR));
+
+        if ($json !== $expected) {
+            if (json_decode($json)) {
+                echo "Expected: $expected\n";
+                echo "Actual: $json\n";
+            }
+
+            throw new \Exception('Expected request result does not match actual result.');
+        }
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function resetDb()
+    {
+        @unlink(dirname(__DIR__) . '/../TestApplication/var/data.db');
+
+        $entityManager = $this->getContainer()->get('doctrine')->getManager();
+        $metadatas = $entityManager->getMetadataFactory()->getAllMetadata();
+        $schemaTool = new SchemaTool($entityManager);
+        $schemaTool->createSchema($metadatas);
+    }
+}
