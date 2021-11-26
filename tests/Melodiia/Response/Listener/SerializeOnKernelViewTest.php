0afac11cd620398538d99acbe11558fb56057d94
Maxime Veber <nek.dev@gmail.com>
2021-11-26 16:28:48 +0100

    feat: add json-api header
    See issue #46 and https://github.com/swagindustries/Melodiia/issues/46

--- tests/Melodiia/Response/Listener/SerializeOnKernelViewTest.php
+++ tests/Melodiia/Response/Listener/SerializeOnKernelViewTest.php
@@ -13,6 +13,7 @@ use SwagIndustries\Melodiia\Serialization\Context\ContextBuilderChainInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -29,30 +30,16 @@ class SerializeOnKernelViewTest extends TestCase
     /** @var SerializeOnKernelView */
     private $listener;
 
+    /** @var ApiResponse */
+    private $dummyResponse;
+
     public function setUp(): void
     {
         $this->serializer = $this->prophesize(SerializerInterface::class);
         $this->contextChain = $this->prophesize(ContextBuilderChainInterface::class);
         $this->contextChain->buildContext(Argument::cetera())->willReturn([]);
         $this->listener = new SerializeOnKernelView($this->serializer->reveal(), $this->contextChain->reveal());
-    }
-
-    public function tearDown(): void
-    {
-        $this->serializer = null;
-        $this->contextChain = null;
-        $this->listener = null;
-    }
-
-    public function testItSubscribeOnKernelView()
-    {
-        $this->assertArrayHasKey(KernelEvents::VIEW, SerializeOnKernelView::getSubscribedEvents());
-        $this->assertInstanceOf(EventSubscriberInterface::class, $this->listener);
-    }
-
-    public function testItTransformApiResponse()
-    {
-        $response = new class() implements ApiResponse {
+        $this->dummyResponse = new class() implements ApiResponse {
             public function httpStatus(): int
             {
                 return 200;
@@ -63,16 +50,62 @@ class SerializeOnKernelViewTest extends TestCase
                 return [];
             }
         };
-        $this->serializer->serialize($response, Argument::cetera())->shouldBeCalled()->willReturn('"hello"');
+    }
+
+    public function tearDown(): void
+    {
+        $this->serializer = null;
+        $this->contextChain = null;
+        $this->listener = null;
+        $this->dummyResponse = null;
+    }
+
+    public function testItSubscribeOnKernelView()
+    {
+        $this->assertArrayHasKey(KernelEvents::VIEW, SerializeOnKernelView::getSubscribedEvents());
+        $this->assertInstanceOf(EventSubscriberInterface::class, $this->listener);
+    }
+
+    public function testItTransformApiResponse()
+    {
+        $this->serializer->serialize($this->dummyResponse, Argument::cetera())->shouldBeCalled()->willReturn('"hello"');
+        $event = new ViewEvent(
+            $this->prophesize(HttpKernelInterface::class)->reveal(),
+            $this->prophesize(Request::class)->reveal(),
+            HttpKernelInterface::MASTER_REQUEST,
+            $this->dummyResponse
+        );
+
+        $this->listener->onKernelView($event);
+
+        $this->assertInstanceOf(JsonResponse::class, $event->getResponse());
+    }
+
+    public function testItDoesNotTransformSymfonyResponse()
+    {
+        $response = new Response('<h1>Containing HTML?</h1>');
         $event = new ViewEvent(
             $this->prophesize(HttpKernelInterface::class)->reveal(),
             $this->prophesize(Request::class)->reveal(),
             HttpKernelInterface::MASTER_REQUEST,
             $response
         );
+        $this->listener->onKernelView($event);
+        $this->assertSame(null, $event->getResponse());
+    }
+
+    public function testItSendsTheRightContentType()
+    {
+        $this->serializer->serialize($this->dummyResponse, Argument::cetera())->shouldBeCalled()->willReturn('"hello"');
+        $event = new ViewEvent(
+            $this->prophesize(HttpKernelInterface::class)->reveal(),
+            $this->prophesize(Request::class)->reveal(),
+            HttpKernelInterface::MASTER_REQUEST,
+            $this->dummyResponse
+        );
 
         $this->listener->onKernelView($event);
 
-        $this->assertInstanceOf(JsonResponse::class, $event->getResponse());
+        $this->assertEquals($event->getResponse()->headers->get('content-type'), SerializeOnKernelView::CONTENT_TYPE);
     }
 }
